b3b618eba8a57a4910dede46266e580b60545232
unknown <mohamedelbalooty123@gmail.com>
2020-12-02 21:06:20 +0200

    adding colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,10 @@
-//TODO COLORS
-//TODO DATABASE COLLECTION NAME
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const KMainColor = Color(0xFF171f44);
+const KSecondColor = Color(0xFF2661dd);
+const KBorderColor = Color(0xFF2a314f);
+const KWhiteColor = Colors.white;
+const KBlackColor = Colors.black;
+const KGrayColor = Colors.grey;
+// 0xFF373c53
